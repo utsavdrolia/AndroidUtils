ec85f4856dfb583906c5af6b7689ac70c2aa3062
Utsav Drolia <utsavdrolia@gmail.com>
2017-02-23 16:03:26 -0500

    Added connecting to any device on the subnet

--- AndroidUtils/AndroidUtils.py
+++ AndroidUtils/AndroidUtils.py
@@ -2,14 +2,14 @@ import subprocess
 from time import sleep
 
 
-def send_command(command):
+def run_command(command):
     print " ".join(command)
-    subprocess.call(command)
+    return subprocess.check_output(command)
 
 
 def connect(dev):
     command = ["adb", "connect", dev]
-    send_command(command)
+    return run_command(command)
 
 
 def connect_all(devs):
@@ -18,34 +18,47 @@ def connect_all(devs):
         sleep(5)
 
 
+def get_available_devices(subnet, num_devs):
+    devs = []
+    subnet = ".".join(subnet.split(".")[0:2])
+    for last in range(1, 255):
+        dev = subnet + "." + str(last)
+        reply = connect(dev).split()
+        if reply[0] == "connected":
+            devs.append(reply[2])
+            if len(devs) == num_devs:
+                return devs
+    return devs
+
+
 def disconnect():
     command = ["adb", "disconnect"]
-    send_command(command)
+    run_command(command)
 
 
 def kill_server():
     command = ["adb", "kill-server"]
-    send_command(command)
+    run_command(command)
 
 
 def start_server():
     command =["adb", "start-server"]
-    send_command(command)
+    run_command(command)
 
 
 def send_file(local, remote, dev):
     command = ["adb", "-s", dev, "wait-for-device", "push", local, remote]
-    send_command(command)
+    run_command(command)
 
 
 def get_file(remote, local, dev):
     command = ["adb", "-s", dev, "wait-for-device", "pull", remote, local]
-    send_command(command)
+    run_command(command)
 
 
 def rm(remote, dev):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "rm", remote]
-    send_command(command)
+    run_command(command)
 
 
 def get_files(remote, local, devs):
@@ -55,23 +68,23 @@ def get_files(remote, local, devs):
 
 def mkdir(dev, dir):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "mkdir -p", dir]
-    send_command(command)
+    run_command(command)
 
 
 def stopApp(dev, app):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "force-stop", app]
-    send_command(command)
+    run_command(command)
 
 
 def launchApp(dev, app):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-n", app]
-    send_command(command)
+    run_command(command)
 
 
 def send_intent(dev, intent):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "broadcast", "-a"]
     command.extend(intent)
-    send_command(command)
+    run_command(command)
 
 
 def send_intent_to_all(devs, intent):
@@ -91,11 +104,11 @@ def killAppOnAll(devs, app):
 
 def trepn_profiler_launch(dev):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "startservice", "com.quicinc.trepn/.TrepnService"]
-    send_command(command)
+    run_command(command)
     sleep(1)
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "broadcast", "-a", "com.quicinc.trepn.load_preferences",
                "-e", "com.quicinc.trepn.load_preferences_file", "/sdcard/trepn/saved_preferences/pref.pref"]
-    send_command(command)
+    run_command(command)
     sleep(1)
 
 
@@ -107,7 +120,7 @@ def trepn_launch_all(devs):
 def trepn_start_profiling(dev):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "broadcast", "-a", "com.quicinc.trepn.start_profiling",
                "-e", "com.quicinc.trepn.database_file", "log"]
-    send_command(command)
+    run_command(command)
 
 
 def trepn_start_profiling_all(devs):
@@ -117,7 +130,7 @@ def trepn_start_profiling_all(devs):
 
 def trepn_stop_profiling(dev):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "broadcast", "-a", "com.quicinc.trepn.stop_profiling"]
-    send_command(command)
+    run_command(command)
 
 
 def trepn_stop_profiling_all(devs):
@@ -128,7 +141,7 @@ def trepn_stop_profiling_all(devs):
 def trepn_read_logs(dev, log):
     command = ["adb", "-s", dev, "wait-for-device", "shell", "am", "broadcast", "-a", "com.quicinc.trepn.export_to_csv", "-e",
                "com.quicinc.trepn.export_db_input_file", "log", "-e", "com.quicinc.trepn.export_csv_output_file", "out.csv"]
-    send_command(command)
+    run_command(command)
     sleep(5)
     get_file("/sdcard/trepn/out.csv", log, dev)
     rm("/sdcard/trepn/out.csv", dev)
